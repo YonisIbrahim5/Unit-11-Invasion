763439ad5aa1ce02db2afdabb045fda4a371fb5a
YonisIbrahim5 <yibrahim5@student.cscc.edu>
2025-04-13 22:36:21 -0400

    added rotating trhe ship to the Ship() class

--- ship.py
+++ ship.py
@@ -24,7 +24,10 @@ class Ship:
         self.rect.midbottom = self.boundaries.midbottom
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
         self.arsenal = arsenal
 
     def update(self):
@@ -34,12 +37,36 @@ class Ship:
 
     def _update_ship_arsenal(self):
         temp_speed = self.settings.ship_speed
+        
         if self.moving_right and self.rect.right < self.boundaries.right:
             self.x += temp_speed
         if self.moving_left and self.rect.left > self.boundaries.left:
             self.x -= temp_speed
+        if self.moving_up and self.rect.top < self.boundaries.top:
+            self.x -= temp_speed
+        if self.moving_down and self.rect.bottom > self.boundaries.bottom:
+            self.x += temp_speed
         
         self.rect.x = self.x
+        self.rect.y = self.y
+        self._rotate_ship()
+
+    def _rotate_ship(self):
+
+        angle = 0
+
+        if self.moving_up:
+            angle = 270
+        elif self.moving_down:
+            angle = 90
+        elif self.moving_left:
+            angle = 180
+        elif self.moving_right:
+            angle = 0
+        
+        self.image = pygame.transform.rotate(self.settings.ship_original_image, angle)
+        self.rect = self.image.get_rect(center=self.rect.center)
+
      
     def draw(self):
         self.arsenal.draw()
