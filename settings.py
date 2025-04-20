a081de9ea63f58bd6e4e8b84be0052963726f0cf
YonisIbrahim5 <yibrahim5@student.cscc.edu>
2025-04-20 18:03:46 -0400

    added new sound effect, and sprites

--- settings.py
+++ settings.py
@@ -7,15 +7,15 @@ class Settings:
         self.screen_w = 1200
         self.screen_h = 800
         self.FPS = 60
-        self.bg_file = Path.cwd() / 'Assets' / 'images' / 'Starbasesnow.png'
+        self.bg_file = Path.cwd() / 'Assets' / 'images' / 'spaceBG.jpg'
 
         self.ship_file = Path.cwd() / 'Assets' / 'images' / 'PurpleShip.png'
         self.ship_file_w = 40
         self.ship_file_h = 60
         self.ship_speed = 5
 
-        self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'laserBlast.png'
-        self.laser_sound = Path.cwd() / 'Assets' / 'sound' / 'laser.mp3'
+        self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'blueLaser.png'
+        self.laser_sound = Path.cwd() / 'Assets' / 'sound' / 'scifiLaser.mp3'
         self.bullet_w = 25
         self.bullet_h = 80
         self.bullet_speed = 7
