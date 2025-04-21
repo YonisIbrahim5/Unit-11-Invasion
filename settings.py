0945e5b1d71821936224f614f674cf5202fc5ed6
YonisIbrahim5 <yibrahim5@student.cscc.edu>
2025-04-20 23:06:38 -0400

    added game stats, game resets only when loses ship

--- settings.py
+++ settings.py
@@ -13,6 +13,7 @@ class Settings:
         self.ship_file_w = 40
         self.ship_file_h = 60
         self.ship_speed = 5
+        self.starting_ship_count = 3
 
         self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'blueLaser.png'
         self.laser_sound = Path.cwd() / 'Assets' / 'sound' / 'scifiLaser.mp3'
